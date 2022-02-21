dcfbbd1d200190be67936ac2f864c3ff6def4143
saens <songsh5775@gmail.com>
2022-02-22 03:41:32 +0900

    Map Rotation Limit

--- Assets/Scripts/MapRotate/MapRotateController.cs
+++ Assets/Scripts/MapRotate/MapRotateController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class MapRotateController : MonoBehaviour
 {
     public DragEvent dragEvent;
-    public Transform map;
+    public Transform horizontalPivot;
+    public Transform verticalPivot;
 
-    public float rotationSpeed;
+    public int rotationLimit = 90;
+    public float rotationSpeed = 1;
 
     public void OnEnable()
     {
@@ -20,8 +22,15 @@ public class MapRotateController : MonoBehaviour
     public void OnDrag(Vector2 dragVector)
     {
         float vertical = (dragVector.y) * 180 / Screen.width * rotationSpeed; // x축 회전 담당
-        float horizontal = (dragVector.x) * 90 / Screen.height * rotationSpeed; // z축 회전 담당
+        float horizontal = -(dragVector.x) * 90 / Screen.height * rotationSpeed; // z축 회전 담당
 
-        map.transform.Rotate(vertical, 0, horizontal);
+        float rotatedRotationV = verticalPivot.transform.rotation.eulerAngles.x + vertical;
+        float rotatedRotationH = horizontalPivot.transform.rotation.eulerAngles.z + horizontal;
+
+        if(rotatedRotationV > 360-rotationLimit || rotatedRotationV < rotationLimit)
+            verticalPivot.transform.Rotate(vertical, 0, 0, Space.Self);
+        if(rotatedRotationH > 360 - rotationLimit || rotatedRotationH < rotationLimit)
+            horizontalPivot.transform.Rotate(0, 0, horizontal, Space.Self);
+        
     }
 }
