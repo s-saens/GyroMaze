624d1be97a671a5fcb02be9cd3644922b27604cc
saens <songsh5775@gmail.com>
2022-03-08 01:04:22 +0900

    Drag/Zoom Input Class 분리

--- Assets/2_Scripts/Inputs/DragInput.cs
+++ Assets/2_Scripts/Inputs/DragInput.cs
@@ -1,59 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragInput : MonoBehaviour, IDragHandler, IBeginDragHandler
+public class DragInput : MonoBehaviour
 {
     public DragEvent dragEvent;
-    public ZoomEvent zoomEvent;
 
-    Vector3 lastPos;
-    Vector3 draggingPos;
-
-    private bool isTwoTouches = false;
 
     private void Update()
     {
-        isTwoTouches = (Input.touchCount == 2);
+        if (Input.touchCount != 1) return;
 
-        if(isTwoTouches)
-        {
-            Touch t1 = Input.GetTouch(0);
-            Touch t2 = Input.GetTouch(1);
+        Touch t = Input.touches[0];
+        Vector2 draggingVector = t.deltaPosition;
 
-            Vector3 t1Tot2 = t2.position - t1.position;
-            float dotProductDelta = Vector3.Dot(t1.deltaPosition, t2.deltaPosition);
-            bool isPinch = dotProductDelta <= 0;
-
-            if(isPinch)
-            {
-                if(Vector3.Dot(t1Tot2, t1.deltaPosition) > 0) // 축소 -> y값 증가
-                {
-                    zoomEvent.OnZoom?.Invoke(dotProductDelta/1920); // 음수값 넘겨중
-                }
-                else if (Vector3.Dot(t1Tot2, t1.deltaPosition) < 0) // 확대
-                {
-                    zoomEvent.OnZoom?.Invoke(-dotProductDelta/1920);
-                }
-            }
-
-        }
-        
-    }
-
-    public void OnBeginDrag(PointerEventData beginPoint)
-    {
-        lastPos = beginPoint.position;
-    }
-    public void OnDrag(PointerEventData draggingPoint)
-    {
-        if(isTwoTouches) return;
-
-        draggingPos = draggingPoint.position;
-
-        Vector2 dragVector = draggingPos - lastPos;
-
-        lastPos = draggingPos;
-
-        dragEvent.OnDrag?.Invoke(dragVector);
+        dragEvent.OnDrag.Invoke(draggingVector);
     }
 }
---
+++ Assets/2_Scripts/Inputs/ZoomInput.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ZoomInput : MonoBehaviour
+{
+    public ZoomEvent zoomEvent;
+
+    Vector3 lastPos;
+    Vector3 draggingPos;
+
+    private bool touchStarted = false;
+    private float lastDistance = 0;
+
+    private void Update()
+    {
+        if(Input.touchCount != 2)
+        {
+            touchStarted = false;
+            return;
+        }
+
+        Touch t1 = Input.touches[0];
+        Touch t2 = Input.touches[1];
+
+        if(!touchStarted)
+        {
+            lastDistance = Vector2.Distance(t1.position, t2.position) / 1920;
+            touchStarted = true;
+        }
+
+        float nowDistance = Vector2.Distance(t1.position, t2.position) / 1920;
+
+        float deltaDistance = nowDistance - lastDistance;
+
+        if(deltaDistance > 0) // 두 점 사이가 멀어짐 : 축소
+        {
+            zoomEvent.OnZoom.Invoke(deltaDistance);
+        }
+        else if(deltaDistance < 0) // 두 점 사이가 가까워짐 : 확대
+        {
+            zoomEvent.OnZoom.Invoke(deltaDistance);
+        }
+    }
+}
