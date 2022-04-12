848c7dbdd46dc610a55e697b4b93f30527c1069c
saens <songsh5775@gmail.com>
2022-04-12 19:01:29 +0900

    remove related to firebase

--- Assets/3_ScriptableObjects/Data/Scripts/User/UserData.cs
+++ Assets/3_ScriptableObjects/Data/Scripts/User/UserData.cs
@@ -1,7 +1,4 @@
 using UnityEngine;
-using Firebase.Auth;
-using Firebase.Database;
-
 
 public class User
 {
@@ -10,16 +7,5 @@ public class User
 
 public static class UserData
 {
-    private static FirebaseAuth auth = FirebaseAuth.DefaultInstance;
-    private static FirebaseDatabase database = FirebaseDatabase.DefaultInstance;
-
-    private static FirebaseUser user
-    {
-        get { return auth.CurrentUser; }
-    }
-
-    private static DatabaseReference userRef // Database에서 user 오브젝트 가져오기
-    {
-        get { return database.GetReference(user.UserId); }
-    }
+    
 }
