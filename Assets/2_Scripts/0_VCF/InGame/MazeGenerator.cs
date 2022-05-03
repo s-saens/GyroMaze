f7127f77dcb10c56decb84a5b6e4e9062262f818
saens <songsh5775@gmail.com>
2022-05-03 13:11:14 +0900

    Update MazeGenerator.cs

--- Assets/2_Scripts/0_VCF/InGame/MazeGenerator.cs
+++ Assets/2_Scripts/0_VCF/InGame/MazeGenerator.cs
@@ -6,10 +6,8 @@ public class Maze
     public int sizeX, sizeY;
     public bool[,] horizontalWalls; // [Y+1][X]
     public bool[,] verticalWalls; // [Y][X+1]
-    public int[,] distances; // [Y][X]
     public int startX, startY;
     public int endX, endY;
-    public int maxDistance = 0;
 
     public Maze(int sizeX, int sizeY)
     {
@@ -18,11 +16,9 @@ public class Maze
 
         horizontalWalls = new bool[sizeY + 1, sizeX];
         verticalWalls = new bool[sizeY, sizeX + 1];
-        distances = new int[sizeY, sizeX];
 
         for(int y=0 ; y<sizeY + 1 ; ++y) for(int x=0 ; x<sizeX ; ++x) horizontalWalls[y, x] = true;
         for(int y=0 ; y<sizeY ; ++y) for(int x=0 ; x<sizeX + 1 ; ++x) verticalWalls[y, x] = true;
-        for(int y=0 ; y<sizeY ; ++y) for(int x=0 ; x<sizeX ; ++x) distances[y, x] = 0;
     }
 }
 
@@ -32,10 +28,21 @@ public class MazeGenerator
     private int[] dy = new int[4] { -1, 1, 0, 0 };
 
     private Maze maze;
+    private int[,] distances; // [Y][X]
+    private int maxDistance;
+
+    private void InitDistances(int sizeY, int sizeX)
+    {
+        maxDistance = 0;
+        distances = new int[sizeY, sizeX];
+        for (int y = 0; y < sizeY; ++y) for (int x = 0; x < sizeX; ++x) distances[y, x] = 0;
+    }
 
     public Maze MakeMazeDFS(int sizeX, int sizeY, int startX, int startY)
     {
         maze = new Maze(sizeX, sizeY);
+
+        InitDistances(sizeX, sizeY);
 
         DFS(startX, startY, 1);
 
@@ -44,27 +51,26 @@ public class MazeGenerator
 
     private void DFS(int x, int y, int dist)
     {
-        maze.distances[y, x] = dist;
-        if(dist >= maze.maxDistance)
+        distances[y, x] = dist;
+        if(dist >= maxDistance)
         {
-            maze.maxDistance = dist;
+            maxDistance = dist;
             maze.endX = x;
             maze.endY = y;
         }
 
         List<int> directionToGo = new List<int>() {0, 1, 2, 3};
-        int cnt = 4;
-        while(cnt > 0)
+        for(int i=4 ; i>0 ; --i)
         {
-            int index = Random.Range(0, cnt);
+            int index = Random.Range(0, i);
             int dir = directionToGo[index];
             directionToGo.RemoveAt(index);
-            cnt--;
 
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
-            if (nx < 0 || nx >= maze.sizeX || ny < 0 || ny >= maze.sizeY || maze.distances[ny, nx] > 0) continue;
+            if (nx < 0 || nx >= maze.sizeX || ny < 0 || ny >= maze.sizeY) continue;
+            if (distances[y,x] > 0) continue;
 
             // 벽 파괴
             if (dir == 0) {maze.horizontalWalls[y, x] = false;}
